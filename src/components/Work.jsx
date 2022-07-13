b2d44bf5b4b44e2f0bc8e6bd21c399012b4aeabb
soyjb <thejulianbeltran@gmail.com>
2022-07-13 18:09:29 -0400

    added Netflix Project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import WorkImg from "../assets/workImg.jpeg";
 import realEstate from "../assets/realestate.jpg";
-import cryptoBase from "../assets/cryptobase.png"
+import cryptoBase from "../assets/cryptobase.png";
+import netflixsite from '../assets/netflixsite.png';
 
 const Work = () => {
   return (
@@ -42,21 +43,21 @@ const Work = () => {
           </div>
 
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${netflixsite})` }}
             className=" shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className=" opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                Netflix React App
               </span>
               <div className=" pt-8 text-center">
-                <a href="/">
+                <a href="https://netflix-react-e2aff.web.app/" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/soyjb/netflix-react" target='_blank'>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
